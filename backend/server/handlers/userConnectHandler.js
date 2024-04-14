489632e85fa5d4b4c3f0157ef4c0d21144536294
Javaughn Pryce <javanpryce1@gmail.com>
2024-04-14 15:42:55 -0400

    fixed user keep reconnecting

--- backend/server/handlers/userConnectHandler.js
+++ backend/server/handlers/userConnectHandler.js
@@ -6,7 +6,10 @@ const logger = require("../config/logger.config");
 module.exports = async function (emitter, data, pubClient) {
   try {
     const token = await sql.verifyToken(data.token);
-    if (!token) return emitter.emit("error", { message: "Invalid token" });
+
+    if (!token)
+      return emitter.emit("tokenNotValid", { message: "Invalid token" });
+
     const user = await sql.getUser(data.username);
     const joinedUser = {
       id: data.id,
@@ -14,24 +17,20 @@ module.exports = async function (emitter, data, pubClient) {
       image: user.image,
       username: user.username,
     };
+
     const users = await getUsers(pubClient);
     const onlineUser = users.find((user) => user.username === data.username);
+
     if (onlineUser) {
       onlineUser.id = data.id;
-      emitter.emit("userData", user);
+      emitter.to(onlineUser.id).emit("userData", onlineUser);
     } else {
       users.push(joinedUser);
-      emitter.emit("userData", joinedUser);
+      emitter.to(joinedUser.id).emit("userData", joinedUser);
     }
-    await setUsers(users, pubClient);
 
-    // if (!user) {
-    //   users.push(joinedUser);
-    // }
-    // emitter.emit("userData", joinedUser);
-    // setUsers(users, pubClient);
+    await setUsers(users, pubClient);
   } catch (err) {
-    emitter.emit("error", { message: "Invalid token" });
     logger.error(err);
   }
 };
